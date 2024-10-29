13edb9e3386fada8df08b6531baffc108bf4f500
Shinjirou <htooagwai9@gmail.com>
2024-10-29 20:21:08 +0630

    Add initial pseudocode in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+Pseudocode
+    CREATE variable container for grid-container
+    CREATE variable boxes for each boxes inside the container
+    CREATE 16x16 grid of div boxes 
+
+    Boxes creation
+        CREATE 16 divs each with 16 vertical boxes inside 
+        ASSIGN each with class name (box)
+        ADD black border to each box
+        ADD mouse hover event
+
+    Mouse Hover Event
+        IF mouse pointer is over the element
+            SET the color of the box black
+*/
